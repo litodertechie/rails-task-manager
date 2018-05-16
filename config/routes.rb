7447377c4e9fd46bb73309050a5c0ff647d01997
Emanuel Coen <emanuelcoen@gmail.com>
2018-05-16 11:41:26 +0200

    Exercise yesterday

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #display all tasks
+  get '/tasks', to: 'tasks#index'
+  #new
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  #display specific task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  #create new task
+  post '/tasks', to: 'tasks#create'
+
+  #edit one task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update', as: :update_task
+
+  #delete a restaurant
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
